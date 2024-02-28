e21ec3eed8847c2fc002575c8bf27a39425b08f7
Jaroslav Dockal <jarin.doc@gmail.com>
2024-02-28 21:06:40 +0100

    Přidáná cesta pro soubor k zápisu

--- src/com/engeto/ja/Settings.java
+++ src/com/engeto/ja/Settings.java
@@ -2,11 +2,16 @@ package com.engeto.ja;
 
 public class Settings {
 
-    private static final String FILE_NAME = "resources/kvetiny.txt";
+    private static final String FILE_NAME_IN = "resources/kvetiny.txt";
+    private static final String FILE_NAME_OUT = "resources/kvetiny_out.txt";
     private static final String DELIMITER = "\t";
 
-    public static String getFileName() {
-        return FILE_NAME;
+    public static String getFileNameIn() {
+        return FILE_NAME_IN;
+    }
+
+    public static String getFileNameOut() {
+        return FILE_NAME_OUT;
     }
 
     public static String getDelimiter() {
